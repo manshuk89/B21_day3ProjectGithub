8c9d7be333dd3928ba8a4fba46af13f4c2b7b0c1
manshuk89 <manshuk.karasaeva@gmail.com>
2020-11-12 16:53:47 -0500

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello Git Last day");
         //comments are coming
+        //second comment
+        // B21 is successful testers
     }
 }
